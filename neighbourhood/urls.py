058b880a4d71a277601d05ab07452b1db4e3a4f7
Francisflow <francisngigi948@gmail.com>
2022-04-18 21:02:05 +0300

    Created login view function and its template to authenticate users

--- neighbourhood/urls.py
+++ neighbourhood/urls.py
@@ -5,5 +5,6 @@ urlpatterns=[
 
     path('', views.home, name='home'),
     path('register/', views.register_request, name='register'),
+    path('login/', views.login_request, name='login'),
 
 ]
--- neighbourhood/views.py
+++ neighbourhood/views.py
@@ -1,7 +1,11 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from .forms import NewUserForm
+from django.contrib.auth import login, authenticate, logout
+from django.contrib import messages
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.decorators import login_required
 # Create your views here.
-
+@login_required(login_url='/login/')
 def home(request):
     return render(request, 'home.html')
 
@@ -17,3 +21,26 @@ def register_request(request):
         messages.error(request, "User registration failed. Invalid information")
     form=NewUserForm()
     return render(request=request, template_name="main/register.html", context={"register_form":form})
+
+
+def login_request(request):
+    if request.method == "POST":
+        form=AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            username=form.cleaned_data["username"]
+            password=form.cleaned_data["password"]
+            user = authenticate(username=username, password=password)
+
+            if user is not None:
+                login(request, user)
+                messages.info(request, f'You are now logged in as {username}.')
+                return redirect("/")
+
+            else:
+                messages.error(request, "Invalid username or password.")
+            
+        else:
+            messages.error(request, "Invalid username or password.")
+    form=AuthenticationForm()
+    return render(request=request, template_name="main/login.html", context={"login_form":form})
+    
