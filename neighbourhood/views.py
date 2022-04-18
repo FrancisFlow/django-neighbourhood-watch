596cdbac130ecea55e1b3fe3942a968d9f20bfe6
Francisflow <francisngigi948@gmail.com>
2022-04-18 20:46:50 +0300

    Created a view function to register users and added email to usercreationform

--- neighbourhood/views.py
+++ neighbourhood/views.py
@@ -4,3 +4,16 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'home.html')
+
+
+def register_request(request):
+    if request.method=='POST':
+        form=NewUserForm(request.POST)
+        if form.is_valid():
+            user=form.save()
+            login(request, user)
+            messages.success(request, "User registration successful.")
+            return redirect("/login")
+        messages.error(request, "User registration failed. Invalid information")
+    form=NewUserForm()
+    return render(request=request, template_name="main/register.html", context={"register_form":form})
