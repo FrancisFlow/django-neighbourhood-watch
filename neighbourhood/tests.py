f6945198f3e0ef3d3d8469beb00749d0fdcd53f8
Francisflow <francisngigi948@gmail.com>
2022-04-19 11:30:38 +0300

    Created unittests for post class

--- neighbourhood/tests.py
+++ neighbourhood/tests.py
@@ -1,5 +1,5 @@
 from django.test import TestCase
-from .models import NeighbourHood, Business
+from .models import NeighbourHood, Business, Post
 from django.contrib.auth.models import User
 # Create your tests here.
 
@@ -29,3 +29,22 @@ class BusinessTestClass(TestCase):
         self.business.delete_business()
         businesses = Business.objects.all()
         self.assertTrue(len(businesses) == 0)
+
+
+class PostTestClass(TestCase):
+    def setUp(self):
+        self.post = Post(title='Test Post')
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.post, Post))
+
+    def test_save_method(self):
+        self.post.save()
+        posts = Post.objects.all()
+        self.assertTrue(len(posts) > 0)
+
+    def test_delete_method(self):
+        self.post.save()
+        self.post.delete_post()
+        posts = Post.objects.all()
+        self.assertTrue(len(posts) == 0)
